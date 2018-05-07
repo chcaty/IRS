7c0cb0907149bf5f8e6e86a826f3ab02c662b46a
chcaty <3161608168@qq.com>
2018-05-07 11:36:38 +0800

    由NQ问题约束求解修改为优先级排班算法

---
+++ IRS.ConsoleApp/DutyTime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRS.ConsoleApp
+{
+    public class DutyTime
+    {
+        private String time;
+
+        private List<User> freeUsers = new List<User>();
+
+        private List<User> dutyUsers = new List<User>();
+
+        public DutyTime() { }
+
+        public DutyTime(string time)
+        {
+            this.time = time;
+        }
+
+        public List<User> GetFreeUsers()
+        {
+            return freeUsers;
+        }
+
+        public List<User> GetDutyUsers()
+        {
+            return dutyUsers;
+        }
+
+        public string GetTime()
+        {
+            return time;
+        }
+    }
+}
---
+++ IRS.ConsoleApp/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRS.ConsoleApp
+{
+    public class User
+    {
+        private String name;
+        private int num;
+
+        public User() { }
+        public User(String name,int num)
+        {
+            this.name = name;
+            this.num = num;
+        }
+
+        public int GetNum()
+        {
+            return num;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
